4f7e27ac57f249ab4070a3f353720dda84241335
Alex PG <aleperaltagar@gmail.com>
2021-05-25 17:43:48 +0200

    function to read a file and store it on an arraylist of strings

--- app/src/main/java/com/aleperaltagar/simpleworkoutnotebook/IODatabaseManager.java
+++ app/src/main/java/com/aleperaltagar/simpleworkoutnotebook/IODatabaseManager.java
@@ -1,9 +1,11 @@
 package com.aleperaltagar.simpleworkoutnotebook;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Environment;
+import android.provider.DocumentsContract;
 import android.util.Log;
 
 import androidx.core.content.FileProvider;
@@ -31,36 +33,52 @@ public class IODatabaseManager {
 
     public void exportDatabase() {
         try {
+            // Get the current date for the file name
             String dateString = new SimpleDateFormat("yyyy_MM_dd").format(Calendar.getInstance().getTime());
+
+            // Create the file in the local files directory
             File path = context.getFilesDir();
             File file = new File(path, dateString + "_db.csv");
             FileOutputStream stream = new FileOutputStream(file);
-            SetsConverter setsConverter = new SetsConverter();
-            String textToFile = "";
 
+            // Set the converted to convert the sets to json
+            SetsConverter setsConverter = new SetsConverter();
+
+            // Load the whole database in a variable
             ArrayList<Exercise> wholeDatabase = Utils.getAllItems(context);
+
+            // Save every exercise in a string variable
+            String textToFile = "";
             for (Exercise e : wholeDatabase) {
                 textToFile += e.getId() + ";" + e.getName() + ";" + e.getCalendar().getTimeInMillis() + ";" + setsConverter.setsToJson(e.getSets()) + ";\n";
             }
             stream.write(textToFile.getBytes());
             stream.close();
 
+            // Prepare the file provider with the file URI
             Uri uri = FileProvider.getUriForFile(context, "com.aleperaltagar.simpleworkoutnotebook.provider", file);
 
+            // Create and launch the intent to share the file
             Intent sendIntent = new Intent();
             sendIntent.setAction(Intent.ACTION_SEND);
             sendIntent.putExtra(Intent.EXTRA_STREAM, uri);
             sendIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
             sendIntent.setType("text/plain");
-
             Intent shareIntent = Intent.createChooser(sendIntent, "Share database");
             shareIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             context.startActivity(shareIntent);
 
         } catch (IOException error) {
-
+            Log.d(TAG, "exportDatabase: " + error);
         }
-
     }
+
+//    public void importDatabase() {
+//        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+//        intent.addCategory(Intent.CATEGORY_OPENABLE);
+//        intent.setType("text/plain");
+//        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//        ((Activity) context).startActivityForResult(intent, READ_REQUEST_CODE);
+//    }
 
 }
--- app/src/main/java/com/aleperaltagar/simpleworkoutnotebook/MainActivity.java
+++ app/src/main/java/com/aleperaltagar/simpleworkoutnotebook/MainActivity.java
@@ -1,6 +1,7 @@
 package com.aleperaltagar.simpleworkoutnotebook;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -8,7 +9,14 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.Toast;
 
@@ -16,13 +24,24 @@ import com.aleperaltagar.simpleworkoutnotebook.DatabaseFiles.ExercisesDatabase;
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.navigation.NavigationView;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "MainActivity";
     private DrawerLayout drawer;
     private NavigationView navigationView;
     private MaterialToolbar toolbar;
+
+    private static final int READ_REQUEST_CODE = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -77,7 +96,7 @@ public class MainActivity extends AppCompatActivity {
                         break;
                     case R.id.about:
                         IODatabaseManager ioDatabaseManager = new IODatabaseManager(getApplicationContext());
-                        ioDatabaseManager.exportDatabase();
+                        importDatabase();
                         break;
                     default:
                         break;
@@ -93,5 +112,52 @@ public class MainActivity extends AppCompatActivity {
         drawer = findViewById(R.id.drawer);
         navigationView = findViewById(R.id.navigationView);
         toolbar = findViewById(R.id.toolbar);
+    }
+
+    private void importDatabase() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("text/plain");
+        startActivityForResult(intent, READ_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == READ_REQUEST_CODE && resultCode == RESULT_OK) {
+            ArrayList<String> text = new ArrayList<>();
+            if (data != null) {
+                Uri uri = data.getData();
+                try {
+                    InputStream inputStream = getContentResolver().openInputStream(uri);
+                    BufferedReader r = new BufferedReader(new InputStreamReader(inputStream));
+                    String mLine;
+                    while ((mLine = r.readLine()) != null) {
+                        text.add(mLine);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public String getPath(Uri uri) {
+
+        String path = null;
+        String[] projection = { MediaStore.Files.FileColumns.DATA };
+        Cursor cursor = getContentResolver().query(uri, projection, null, null, null);
+
+        if(cursor == null){
+            path = uri.getPath();
+        }
+        else{
+            cursor.moveToFirst();
+            int column_index = cursor.getColumnIndexOrThrow(projection[0]);
+            path = cursor.getString(column_index);
+            cursor.close();
+        }
+
+        return ((path == null || path.isEmpty()) ? (uri.getPath()) : path);
     }
 }
