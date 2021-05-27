a023b8c05a4d907cbfe941ed4b1a0f2c58267e52
Alex PG <aleperaltagar@gmail.com>
2021-05-27 20:39:28 +0200

    couple of corrections from previous commit

--- app/src/main/java/com/aleperaltagar/simpleworkoutnotebook/ListOfExercisesFragment.java
+++ app/src/main/java/com/aleperaltagar/simpleworkoutnotebook/ListOfExercisesFragment.java
@@ -17,14 +17,6 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import org.apache.commons.text.similarity.CosineDistance;
-import org.apache.commons.text.similarity.JaccardDistance;
-import org.apache.commons.text.similarity.JaccardSimilarity;
-import org.apache.commons.text.similarity.LevenshteinDistance;
-import org.apache.commons.text.similarity.SimilarityScore;
-import org.apache.commons.text.similarity.SimilarityScoreFrom;
-import org.w3c.dom.Text;
-
 import java.util.ArrayList;
 
 public class ListOfExercisesFragment extends Fragment {
@@ -84,24 +76,6 @@ public class ListOfExercisesFragment extends Fragment {
         listOfExercisesAdapter.setItems(foundItems);
     }
 
-    private void searchExerciseOld(String txtExercise){
-        if (txtExercise.equals("")) {
-            listOfExercisesAdapter.setItems(exercises);
-        } else {
-            txtExercise = txtExercise.toLowerCase();
-            ArrayList<String> foundItems = new ArrayList<>();
-            JaccardSimilarity comparator = new JaccardSimilarity();
-            for (String exercise : exercises) {
-                String exerciseLower = exercise.toLowerCase();
-                double score = comparator.apply(txtExercise,  exerciseLower);
-                if (score > 0.4) {
-                    foundItems.add(exercise);
-                }
-            }
-            listOfExercisesAdapter.setItems(foundItems);
-        }
-    }
-
     private void initViews(View view) {
         listOfExercisesRecView  = view.findViewById(R.id.listOfExercisesFragmentRecview);
         txtSearchExercise = view.findViewById(R.id.txtSearchExercise);
@@ -115,8 +89,6 @@ public class ListOfExercisesFragment extends Fragment {
         listOfExercisesRecView.setLayoutManager((new LinearLayoutManager(getActivity(), RecyclerView.VERTICAL, false)));
 
         exercises = Utils.getUniqueItemsString(getActivity());
-        if (null != exercises) {
-            listOfExercisesAdapter.setItems(exercises);
-        }
+        listOfExercisesAdapter.setItems(exercises);
     }
 }
